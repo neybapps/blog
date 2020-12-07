d1af1b03db61a5f2e848531f4157e118a956814f
NeybApps <neybapps@gmail.com>
2020-12-06 18:45:27 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 module.exports = {
   pathPrefix: '/blog',
   siteMetadata: {
-    siteUrl: 'https://www.apollographql.com/blog'
+    siteUrl: 'https://wikiexplain-blog.netlify.app'
   },
   plugins: [
     'gatsby-plugin-svgr',
